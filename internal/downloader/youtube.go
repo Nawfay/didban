479d12a76e3cb7d0fa5c1253af9073c34c0ade68
Nawaf <dishit79@gmail.com>
2025-06-17 00:08:55 -0400

    small updates

--- internal/downloader/youtube.go
+++ internal/downloader/youtube.go
@@ -83,27 +83,13 @@ func DownloadVideoYt(client *Client, videoID string, tmpPath string, output stri
 
 
 func ExampleClient() {
-// 	videoID := "0k00mxCZtcQ"
-	
-// 	client := NewClient()
-
-
-// 	err1, err2 := DownloadVideoYt(client, videoID, "/home/nawaf/Documents/GitHub/didban/tmp", "output.mp3")
-// 	if err1 != false {
-// 		fmt.Println("Error downloading video:", err1)
-// 	} else {
-// 		fmt.Println("Video downloaded successfully:", err2)
-// 	}
-// 	// Output: Video downloaded successfully: <nil>
-// 	// fmt.Println("Video downloaded successfully:", err2)
-// }
 
 	fmt.Println("This is an example function for the Client struct.")
 
 	client := NewClient()
-	track, _ := logic.FetchTrack(3383978371)
-	videoID, _ := logic.DeezerToYtResolver(3383978371)
-	err1, err2 := DownloadVideoYt(client, videoID, "/home/nawaf/Documents/GitHub/didban/tmp", "output.mp3", *track)
+	track, _ := logic.FetchTrack(77450636)
+	videoID, _ := logic.DeezerToYtResolver(77450636)
+	err1, err2 := DownloadVideoYt(client, videoID, "/home/nawaf/Documents/GitHub/didban/tmp", fmt.Sprintf("%s - %s.mp3", track.Title, track.Artist.Name), *track)
 	
 	if err1 != false {
 		fmt.Println("Error downloading video:", err1)
--- internal/models/deezer.go
+++ internal/models/deezer.go
@@ -3,12 +3,13 @@ package models
 
 type Track struct {
 	Title  string `json:"title"`
+	ReleaseDate string `json:"release_date"`
 	Duration int    `json:"duration"`
 	Artist struct {
 		Name string `json:"name"`
 	} `json:"artist"`
 	Album struct {
 		Title string `json:"title"`
-		Cover string `json:"cover_medium"`
+		Cover string `json:"cover_big"`
 	} `json:"album"`
 }
